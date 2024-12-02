d46f0e7377d20dac4bc05a2329106e2cfb62fb03
Kar Deepak Kumar <deepakkumaar97@gmail.com>
2024-12-02 13:19:55 +0530

    beautify view stats page

---
+++ lib/utils/stats_style.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+Scaffold gameStatsPageBuilder(List<Map<dynamic, dynamic>> gameStats, String title) {
+  return Scaffold(
+    appBar: AppBar(title: Text(title)),
+    body: gameStats.isEmpty
+      ? statsUnavailableBody()
+      : gameStatsViewBuilder(gameStats),
+  );
+}
+
+ListView gameStatsViewBuilder(List<Map<dynamic, dynamic>> gameStats) {
+  return ListView.builder(
+    itemCount: gameStats.length,
+    itemBuilder: (context, index) {
+      final stat = gameStats[index];
+      return statsContainer(index, stat);
+    },
+  );
+}
+
+Center statsUnavailableBody() {
+  return const Center(
+      child: Text(
+        'No stats available.',
+        style: TextStyle(fontSize: 16, color: Colors.grey),
+      ),
+    );
+}
+
+Container statsContainer(int index, Map<dynamic, dynamic> stat) {
+  final bool isEven = index % 2 == 0;
+
+  return Container(
+    padding: const EdgeInsets.all(12.0),
+    margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+    decoration: statsBoxDecor(isEven),
+    child: statsRow(index, stat),
+  );
+}
+
+BoxDecoration statsBoxDecor(bool isEven) {
+  return BoxDecoration(
+    color: statsColor(isEven),
+    borderRadius: BorderRadius.circular(8.0),
+    boxShadow: statsBox,
+  );
+}
+
+Color statsColor(bool isEven) => isEven ? Colors.blue.shade50 : Colors.white;
+
+List<BoxShadow> get statsBox {
+  return [
+      BoxShadow(
+        color: Colors.grey.shade300,
+        blurRadius: 4.0,
+        offset: const Offset(0, 2),
+      ),
+    ];
+}
+
+Row statsRow(int index, Map<dynamic, dynamic> stat) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(
+        '${index + 1}',
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16.0,
+          color: Colors.blueAccent,
+        ),
+      ),
+      Text(
+        stat['time'],
+        style: const TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      Text(
+        stat['date'],
+        style: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.grey,
+          fontStyle: FontStyle.italic,
+        ),
+      ),
+    ],
+  );
+}
+
--- lib/views/stats/game_2048_stats_page.dart
+++ lib/views/stats/game_2048_stats_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
+import 'package:brain_games/utils/stats_style.dart';
+
 class Game2048StatsPage extends StatelessWidget {
 
   const Game2048StatsPage({super.key});
@@ -13,49 +15,7 @@ class Game2048StatsPage extends StatelessWidget {
         .toList()
         .cast<Map>();
     gameStats.sort((a, b) => a['time'].compareTo(b['time']));
-    return game2048StatsPageBuilder(gameStats);
-  }
-
-  Scaffold game2048StatsPageBuilder(List<Map<dynamic, dynamic>> gameStats) {
-    return Scaffold(
-    appBar: AppBar(title: const Text('2048 Stats')),
-    body: gameStats.isEmpty
-      ? statsUnavailableBody()
-      : game2048StatsViewBuilder(gameStats),
-  );
-  }
-
-  Center statsUnavailableBody() {
-    return const Center(
-        child: Text(
-          'No stats available.',
-          style: TextStyle(fontSize: 16, color: Colors.grey),
-        ),
-      );
-  }
-
-  ListView game2048StatsViewBuilder(List<Map<dynamic, dynamic>> gameStats) {
-    return ListView.builder(
-      itemCount: gameStats.length,
-      itemBuilder: (context, index) {
-        final stat = gameStats[index];
-        return statsContainer(index, stat);
-      },
-    );
-  }
-
-  Container statsContainer(int index, Map<dynamic, dynamic> stat) {
-    return Container(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('${index + 1}', style: const TextStyle(fontWeight: FontWeight.bold)),
-            Text(stat['time'], style: const TextStyle(fontSize: 16.0)),
-            Text(stat['date'], style: const TextStyle(color: Colors.grey)),
-          ],
-        ),
-      );
+    return gameStatsPageBuilder(gameStats, '2048 stats');
   }
 
 }
